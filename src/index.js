541ad60ef5d9240a46c35a840b23139bf1502e42
WuChenDi <996194720@qq.com>
2021-12-21 21:46:29 +0800

    feat: 添加沾喜气功能处理

--- src/index.js
+++ src/index.js
@@ -7,54 +7,72 @@ const api = require('./lib/api')(COOKIE)
 
 // 获取可抽奖次数
 async function get_raw_time() {
-	const res = await api.get_cur_point()
-	return Math.floor(res / 200)
+  const res = await api.get_cur_point()
+  return Math.floor(res / 200)
 }
 
 // 抽奖一次
 async function draw() {
-	const res = await api.draw()
-	message(`抽奖成功，获得：${res.lottery_name}`)
-	return res
+  const res = await api.draw()
+  const { lottery_name } = res
+  message(`抽奖成功，获得: ${lottery_name}`)
+  return res
 }
 
 // 抽所有
 async function draw_all() {
-	const time = await get_raw_time()
-	message(`梭哈, 可抽奖次数${time}`)
-	if (!time) {
-		message(`抽奖完成`)
-	}
+  const time = await get_raw_time()
+  message(`梭哈, 可抽奖次数${time}`)
+  if (!time) {
+    message(`抽奖完成`)
+  }
 
-	for (let i = 0; i < time; i++) {
-		await draw()
-	}
+  for (let i = 0; i < time; i++) {
+    await draw()
+  }
 
-	if (await get_raw_time()) {
-		draw_all()
-	}
+  if (await get_raw_time()) {
+    await draw_all()
+  }
+}
+
+// 粘喜气
+async function dipLucky() {
+  const RANDOM_NUMBER = Math.floor(Math.random() * 5)
+  const { lotteries } = await api.get_dip_lucky_list()
+  const DIP_LUCKY_ID = lotteries?.[RANDOM_NUMBER]?.history_id ?? 0
+
+  const { has_dip, dip_action, total_value } = await api.dip_lucky(DIP_LUCKY_ID)
+
+  const BeamingValue = `当前喜气值: ${total_value}`
+
+  if (has_dip) return `今日已沾过喜气, ${BeamingValue}`
+
+  if (dip_action === 1) return `沾喜气成功! ${BeamingValue}`
 }
 
 ;(async () => {
-	// 查询今日是否已经签到
-	const today_status = await api.get_today_status()
-	if (today_status) {
-		message('今日已经签到！')
+  // 查询今日是否已经签到
+  const today_status = await api.get_today_status()
+  if (today_status) {
+    message('今日已经签到!')
 
-		// 查询今日是否有免费抽奖机会
-		const { free_count } = await api.lottery_config()
+    // 查询今日是否有免费抽奖机会
+    const { free_count } = await api.lottery_config()
 
-		if (free_count === 0) return message('今日已经免费抽奖！')
+    if (free_count === 0) return message('今日已经免费抽奖!')
 
-		// 去抽奖
-		ALL_IN === 'true' ? draw_all() : draw()
+    // 去抽奖
+    ALL_IN === 'true' ? await draw_all() : await draw()
 
-		return
-	}
+    return
+  }
 
-	api.check_in().then(({ sum_point }) => {
-		message(`签到成功！当前积分: ${sum_point}`)
-		// 去抽奖
-		ALL_IN === 'true' ? draw_all() : draw()
-	})
+  api.check_in().then(({ sum_point }) => {
+    message(`签到成功!当前积分: ${sum_point}`)
+    // 去抽奖
+    ALL_IN === 'true' ? draw_all() : draw()
+  })
+  const dipMsg = await dipLucky()
+  dipMsg(dipMsg)
 })()
